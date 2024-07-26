a12b33c2c3e549d5ff21d8b638511e2d8f037e09
Huipio <101611469+Huipio@users.noreply.github.com>
2024-07-25 18:11:47 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
-$( document ).ready(function() {
-    
+$(document).ready(function() {
     var envelope = $('#envelope');
     var btn_open = $("#open");
     var btn_reset = $("#reset");
-    
-    envelope.click( function() {
+    var message = $(".message");
+    var btn_si = $("#si");
+    var btn_no = $("#no");
+
+    envelope.click(function() {
         open();
     });
-    btn_open.click( function() {
+
+    btn_open.click(function() {
         open();
     });
-    btn_reset.click( function() {
+
+    btn_reset.click(function() {
         close();
     });
 
+    btn_si.click(function() {
+        Fiesta();
+    });
+
+    btn_no.click(function() {
+        disableNoButton();
+    });
+
     function open() {
-        envelope.addClass("open")
-           .removeClass("close");
+        envelope.addClass("open").removeClass("close");
+        message.fadeIn();  // Mostrar el mensaje
     }
+
     function close() {
-        envelope.addClass("close")
-           .removeClass("open");
+        envelope.addClass("close").removeClass("open");
+        message.fadeOut();  // Ocultar el mensaje
+        $("#video-container").hide();
+        $("#youtube-video").attr("src", "");  // Detener el video
     }
-   
+
+    function Fiesta() {
+        var videoID = "6q8jm0Lu7GU";  // Reemplaza esto con el ID de tu video
+        $("#video-container").show();
+        $("#youtube-video").attr("src", "https://www.youtube.com/embed/" + videoID + "?autoplay=1");
+    }
+
+    function disableNoButton() {
+        btn_no.prop("disabled", true).text("No disponible");
+    }
 });
