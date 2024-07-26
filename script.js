f858bbf708ad4a935c4695f25b1e1c4af8a1db78
Huipio <101611469+Huipio@users.noreply.github.com>
2024-07-25 18:22:05 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     var envelope = $('#envelope');
     var btn_open = $("#open");
-    var btn_reset = $("#reset");
     var message = $(".message");
     var btn_si = $("#si");
     var btn_no = $("#no");
@@ -14,16 +13,12 @@ $(document).ready(function() {
         open();
     });
 
-    btn_reset.click(function() {
-        close();
-    });
-
     btn_si.click(function() {
         Fiesta();
     });
 
-    btn_no.click(function() {
-        disableNoButton();
+    btn_no.mouseover(function() {
+        moveNoButton();
     });
 
     function open() {
@@ -40,11 +35,42 @@ $(document).ready(function() {
 
     function Fiesta() {
         var videoID = "6q8jm0Lu7GU";  // Reemplaza esto con el ID de tu video
-        $("#video-container").show();
-        $("#youtube-video").attr("src", "https://www.youtube.com/embed/" + videoID + "?autoplay=1");
+        $(".envlope-wrapper, .reset").fadeOut(function() {
+            $("#video-container").fadeIn();
+            $("#youtube-video").attr("src", "https://www.youtube.com/embed/" + videoID + "?autoplay=1");
+            enterFullScreen(document.getElementById("video-container"));
+        });
     }
 
-    function disableNoButton() {
-        btn_no.prop("disabled", true).text("No disponible");
+    function moveNoButton() {
+        var container = $(".respuesta");
+        var containerWidth = container.width();
+        var containerHeight = container.height();
+        var btnWidth = btn_no.width();
+        var btnHeight = btn_no.height();
+
+        var maxLeft = containerWidth - btnWidth;
+        var maxTop = containerHeight - btnHeight;
+
+        var randomLeft = Math.random() * maxLeft;
+        var randomTop = Math.random() * maxTop;
+
+        btn_no.css({
+            position: 'absolute',
+            left: randomLeft + 'px',
+            top: randomTop + 'px'
+        });
+    }
+
+    function enterFullScreen(element) {
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) { // Firefox
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) { // Chrome, Safari, Opera
+            element.webkitRequestFullscreen();
+        } else if (element.msRequestFullscreen) { // IE/Edge
+            element.msRequestFullscreen();
+        }
     }
 });
